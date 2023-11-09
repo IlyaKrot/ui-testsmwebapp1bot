8abce385ea822c0f16ec6c29d3d68242566436c3
Pokil <iapokutnykh@rusatom-utilities.ru>
2023-11-09 19:10:48 +0300

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <h1>{{ user }}</h1>
+  </div>
+</template>
+
+<script>
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
+
+tg.expand(); //расширяем на все окно  
+
+export default {
+  name: 'Main',
+  data () {
+    return {
+      user: tg.initDataUnsafe.user
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main {
+
+}
+</style>
